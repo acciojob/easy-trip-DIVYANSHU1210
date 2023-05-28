2ec4e4cb844946ad07d6eb2c29c460189fdc7b73
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-05-28 07:13:39 +0530

    8th

--- src/main/java/com/driver/Repositories/AirportRepository.java
+++ src/main/java/com/driver/Repositories/AirportRepository.java
@@ -30,47 +30,47 @@ public class AirportRepository {
 
 
     public String getLargetAirportName() {
-        String answer="";
-        int ans=0;
-        for(String name:Airports.keySet()){
-            int co=Airports.get(name).getNoOfTerminals();
-            if(co>ans){
-                ans=co;
-                answer=name;
-            }
-        }
-        return answer;
-//        int maxTerminals = 0;
-//        String largestAirport = "";
-//        for(String key: Airports.keySet()){
-//            if(Airports.get(key).getNoOfTerminals() > maxTerminals){
-//                maxTerminals = Airports.get(key).getNoOfTerminals();
-//                largestAirport = key;
+//        String answer="";
+//        int ans=0;
+//        for(String name:Airports.keySet()){
+//            int co=Airports.get(name).getNoOfTerminals();
+//            if(co>ans){
+//                ans=co;
+//                answer=name;
 //            }
 //        }
-//        return largestAirport;
+//        return answer;
+        int maxTerminals = 0;
+        String largestAirport = "";
+        for(String key: Airports.keySet()){
+            if(Airports.get(key).getNoOfTerminals() > maxTerminals){
+                maxTerminals = Airports.get(key).getNoOfTerminals();
+                largestAirport = key;
+            }
+        }
+        return largestAirport;
     }
 
     public double getShortestDurationOfPossibleBetweenTwoCities(City fromCity, City toCity) {
-//        double shortestDuration = Integer.MAX_VALUE;
-//        for(Flight flight: Flights.values()){
-//            if(flight.getFromCity().equals(fromCity) && flight.getToCity().equals(toCity)){
-//                if(shortestDuration > flight.getDuration()){
-//                    shortestDuration = flight.getDuration();
-//                }
-//            }
-//        }
-//        if(shortestDuration == Integer.MAX_VALUE)return -1;
-//        return shortestDuration;
-        double duration=Integer.MAX_VALUE;
-        for (Flight flight :Flights.values()){
-            if(fromCity.equals(flight.getFromCity()) && toCity.equals(flight.getToCity())){
-                if(duration>flight.getDuration()){
-                    duration=flight.getDuration();
+        double shortestDuration = Integer.MAX_VALUE;
+        for(Flight flight: Flights.values()){
+            if(flight.getFromCity().equals(fromCity) && flight.getToCity().equals(toCity)){
+                if(shortestDuration > flight.getDuration()){
+                    shortestDuration = flight.getDuration();
                 }
             }
         }
-        return duration==Integer.MAX_VALUE?-1:duration;
+        if(shortestDuration == Integer.MAX_VALUE)return -1;
+        return shortestDuration;
+//        double duration=Integer.MAX_VALUE;
+//        for (Flight flight :Flights.values()){
+//            if(fromCity.equals(flight.getFromCity()) && toCity.equals(flight.getToCity())){
+//                if(duration>flight.getDuration()){
+//                    duration=flight.getDuration();
+//                }
+//            }
+//        }
+//        return duration==Integer.MAX_VALUE?-1:duration;
     }
 
     public void addPassenger(Passenger passenger) {
